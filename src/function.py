1c69b5128053d3f99f88782c84c25cf21f092bf7
alina <khomkolovaalina@gmail.com>
2024-05-07 22:00:46 +0200

    Внесены правки и добавлена функция сортировки и вывода последних 5 выполненных операций.

--- src/function.py
+++ src/function.py
@@ -1,4 +1,6 @@
 import json
+from datetime import datetime
+
 from src.classes import Bank
 from src.settings import OPERATIONS_PATH
 
@@ -9,24 +11,41 @@ def load_operations(path: str = OPERATIONS_PATH):
         operations = []
         for item in data_file:
             date_operation = item.get('date')
-            description_operation = item.get('description')
-            from_operation = item.get('from')
-            to_operation = item.get('to')
-            state_operation = item.get('state')
+            # Сортировка операций по дате (если есть операции с датами)
+            if date_operation is not None:
+                description_operation = item.get('description')
+                from_operation = item.get('from')
+                to_operation = item.get('to')
+                state_operation = item.get('state')
 
-            operation_amount_operation = item.get('operationAmount')
-            amount_operation = item.get('operationAmount', {}).get('amount')
-            code_operation = item.get('operationAmount', {}).get('currency', {}).get('code')
+                operation_amount_operation = item.get('operationAmount')
+                amount_operation = item.get('operationAmount', {}).get('amount') if operation_amount_operation else ""
+                code_operation = item.get('operationAmount', {}).get('currency', {}).get(
+                    'code') if operation_amount_operation else ""
 
-            operation = Bank(state_operation, date_operation,
-                             description_operation, from_operation,
-                             to_operation, amount_operation, code_operation)
+                operation = Bank(state_operation, date_operation,
+                                 description_operation, from_operation,
+                                 to_operation, amount_operation, code_operation)
 
-            operations.append(operation)
+                operations.append(operation)
+
         return operations
 
 
-operations_data = load_operations()
+def sorted_operation_return_last_five():
+    last_five = []
+    operations = load_operations()
+    sorted_operations = sorted(operations, key=lambda x: datetime.strptime(x.date_operation, "%Y-%m-%dT%H:%M:%S.%f"))
+    sorted_operations.reverse()
+    for operation in sorted_operations:
+        if operation.state_operation == "EXECUTED":
+            last_five.append(operation)
+            if len(last_five) == 5:
+                break
+    return last_five
+
+
+operations_data = sorted_operation_return_last_five()
 
 for operation in operations_data:
     bank_instance = Bank(operation.state_operation, operation.date_operation,
