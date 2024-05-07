deffa27f91a302bf0012a1b5de298739dc36396b
alina <khomkolovaalina@gmail.com>
2024-05-07 20:43:41 +0200

    Изменила методы класса и добавила методы.

--- src/classes.py
+++ src/classes.py
@@ -30,10 +30,31 @@ class Bank:
         return f"{date:%d.%m.%Y}"
 
     def formatting_from(self):
-        pass
+        if self.from_operation is None:
+            return ""
+
+        number_values_split = 4
+        card_number = self.from_operation.split()[-1]
+        title_card = " ".join(self.from_operation.split()[:-1])
+
+        private_number = card_number[:6] + (len(card_number[6:-4]) * "*") + card_number[-4:]
+        grouped_private_number = [private_number[i:i + number_values_split] for i in
+                                  range(0, len(private_number), number_values_split)]
+
+        return f"{title_card} {' '.join(grouped_private_number)} -> "
+
+    def formatting_to(self):
+        card_number = self.to_operation.split()[-1]
+        title_card = "".join(self.to_operation.split()[:-1])
+        private_number = (len(card_number[:-4]) * "*") + card_number[-4:]
+        return f"{title_card} {private_number}"
 
     def formatting_string(self):
-        print(f"{self.formatting_data()} {self.description_operation}")
+        if self.to_operation is None:
+            return ""
+        print(f"{self.formatting_data()} {self.description_operation}\n"
+              f"{self.formatting_from()}{self.formatting_to()}\n"
+              f"{self.amount_operation} {self.code_operation}\n")
 
     # def print_init(self):
     #     for i in range(1):
