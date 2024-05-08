236e76a92304139c086f7851916fc20728a8dd41
alina <khomkolovaalina@gmail.com>
2024-05-08 15:23:49 +0200

    Без теста. готовый вывод

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+from src.function import sorted_operation_return_last_five
+from src.operations_with_bank import Bank
+
+
 def main():
+    operations_data = sorted_operation_return_last_five()
+
+    for operation in operations_data:
+        bank_instance = Bank(operation.state_operation, operation.date_operation,
+                             operation.description_operation, operation.from_operation, operation.to_operation,
+                             operation.amount_operation, operation.code_operation)
+        bank_instance.formatting_string()
+
+
+if __name__ == '__main__':
+    main()
--- src/settings.py
+++ src/settings.py
@@ -4,5 +4,5 @@ current_dir = os.getcwd()
 OPERATIONS_PATH = os.path.join("/home/alina/course/Coursework/data", "operations.json")
 
 SRC_PATH = os.path.join("src")
-CLASS_PATH = os.path.join("classes.py")
+CLASS_PATH = os.path.join("operations_with_bank.py")
 FUNCTION_PATH = os.path.join("function.py")
